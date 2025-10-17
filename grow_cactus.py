b84b68b0b96ffd3b223bf0bb18813075678affc9
toxidxd <toxid13@yandex.ru>
2025-10-17 18:12:42 +0300

    drones added

--- grow_cactus.py
+++ grow_cactus.py
@@ -5,13 +5,12 @@
 # print(num_items(Items.Carrot))
 
 def planting_cactus():
-	for i in range(get_world_size()):
-		for j in range(get_world_size()):
-			if can_harvest():
-				harvest()
-			plant(Entities.Cactus)
-			use_item(Items.Water)
-			#use_item(Items.Fertilizer)
-			move(North)
-		move(East)
+	for _ in range(get_world_size()):
+		if can_harvest():
+			harvest()
+		plant(Entities.Cactus)
+		use_item(Items.Water)
+		#use_item(Items.Fertilizer)
+		move(North)
+
 	
