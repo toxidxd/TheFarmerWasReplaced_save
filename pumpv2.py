e91f5adb7b4b9636e24020dcfd25fe98d13fe90f
toxidxd <toxid13@yandex.ru>
2025-10-17 17:07:27 +0300

    cleanup

--- pumpv2.py
+++ pumpv2.py
@@ -3,27 +3,27 @@ def grow_pump():
 	plant(Entities.Pumpkin)		
 
 
-def second_grow():
-	if get_entity_type() != Entities.Pumpkin:
-		plant(Entities.Pumpkin)
-		use_item(Items.Water)
-	elif get_entity_type() == None:
-		plant(Entities.Pumpkin)
-		use_item(Items.Water)
+# def second_grow():
+# 	if get_entity_type() != Entities.Pumpkin:
+# 		plant(Entities.Pumpkin)
+# 		use_item(Items.Water)
+# 	elif get_entity_type() == None:
+# 		plant(Entities.Pumpkin)
+# 		use_item(Items.Water)
 
 
-def regrow_dead_pump():
-	no_dead_pumps = 128
-	while no_dead_pumps > 2:
-		no_dead_pumps = 0
+# def regrow_dead_pump():
+# 	no_dead_pumps = 128
+# 	while no_dead_pumps > 2:
+# 		no_dead_pumps = 0
 
-		if get_entity_type() == Entities.Dead_Pumpkin:
-			plant(Entities.Pumpkin)
-			use_item(Items.Water)
-			no_dead_pumps += 1
-		elif get_entity_type() == None:
-			plant(Entities.Pumpkin)
-			use_item(Items.Water)
+# 		if get_entity_type() == Entities.Dead_Pumpkin:
+# 			plant(Entities.Pumpkin)
+# 			use_item(Items.Water)
+# 			no_dead_pumps += 1
+# 		elif get_entity_type() == None:
+# 			plant(Entities.Pumpkin)
+# 			use_item(Items.Water)
 
 
 def harvest_pumps():
