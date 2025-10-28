c6fa1590a7389d527df50f0ed1058a6a38e3436e
toxidxd <toxid13@yandex.ru>
2025-10-29 01:01:12 +0300

    optimization

--- grow_pumpkin.py
+++ grow_pumpkin.py
@@ -7,10 +7,22 @@ def drone_task():
 
     while True:
 
+        # for _ in range(get_world_size()*3):
+        #     if get_entity_type() != Entities.Pumpkin:
+        #         plant(Entities.Pumpkin)
+        #     elif get_entity_type() == Entities.Dead_Pumpkin:
+        #         plant(Entities.Pumpkin)
+        #
+        #     # if get_water() < 0.70 and num_items(Items.Water) > 1000:
+        #     #     use_item(Items.Water)
+        #
+        #     move(North)
+
         for _ in range(get_world_size()):
             if get_entity_type() != Entities.Pumpkin:
                 plant(Entities.Pumpkin)
-            elif get_entity_type() == Entities.Dead_Pumpkin:
+            elif can_harvest():
+                harvest()
                 plant(Entities.Pumpkin)
 
             if get_water() < 0.70 and num_items(Items.Water) > 1000:
@@ -18,20 +30,8 @@ def drone_task():
 
             move(North)
 
-        # while True:
-        #     for _ in range(get_world_size()):
-        #         if get_entity_type() != Entities.Pumpkin:
-        #             plant(Entities.Pumpkin)
-        #         elif get_entity_type() == Entities.Dead_Pumpkin:
-        #             plant(Entities.Pumpkin)
-        #
-        #         if get_water() < 0.70 and num_items(Items.Water) > 1000:
-        #             use_item(Items.Water)
-        #
-        #         move(North)
-
-
 def planting_pumpkin(need_count=0):
+    harvest()
     clear()
     grow_till_field.go_till()
     while num_items(Items.Pumpkin) < need_count:
